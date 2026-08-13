495a8d24549cdbcdb5619c4531d8ed222cfdc061
KNightING <l.xiang666@gmail.com>
2026-08-13 20:49:31 +0800

    feat(middleware): 重新引入路由中介軟體以處理結尾斜線的重定向

--- app/middleware/00.replacePath.ts
+++ app/middleware/00.replacePath.ts
@@ -1,7 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (import.meta.server) {
-    return
-  }
   if (useRuntimeConfig().public.replaceEndSplash !== true) {
     return
   }
@@ -9,9 +6,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path !== '/' && to.path.endsWith('/')) {
     const path = to.path.substring(0, to.path.length - 1)
     return navigateTo({
+      ...to,
       path,
-      query: to.query,
-      hash: to.hash,
     })
   }
 })
